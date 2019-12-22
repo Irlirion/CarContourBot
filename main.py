8894cfe1e36dff181e303365580577f6450f3c95
Григорьев Ростислав <43699715+Irlirion@users.noreply.github.com>
2019-12-22 10:53:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+!pip install pyTelegramBotAPI
 import telebot
 import cv2 as cv
 import numpy as np
@@ -8,33 +9,21 @@ hsv_max = np.array((26, 238, 255), np.uint8)
 
 
 def get_contour(img):
-    # hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    #
-    # green_low = np.array([45, 100, 50])
-    # green_high = np.array([75, 255, 255])
-    # curr_mask = cv2.inRange(hsv_img, green_low, green_high)
-    # hsv_img[curr_mask > 0] = ([75, 255, 200])
-    #
-    # ## converting the HSV image to Gray inorder to be able to apply
-    # ## contouring
-    # RGB_again = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2RGB)
-    # gray = cv2.cvtColor(RGB_again, cv2.COLOR_RGB2GRAY)
-    #
-    # ret, threshold = cv2.threshold(gray, 90, 255, 0)
-    #
-    # contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #
-    # img = np.full((image.shape[0], image.shape[1], 3), 255, dtype=np.uint8)  # create
-    # cv2.drawContours(img, contours, -1, (0, 0, 0), 3)
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray,(1,1),1000)
+    flag, thresh = cv.threshold(blur, 70, 255, cv.THRESH_BINARY)
+    edged=cv.Canny(thresh,30,200)
+    # Find contours
+    contours, hierarchy = cv.findContours(edged,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv.contourArea,reverse=True) 
+    # Select long perimeters only
+    perimeters = [cv.arcLength(contours[i],True) for i in range(len(contours))]
+    listindex=[i for i in range(int(len(perimeters)/3)) if perimeters[i]>perimeters[0]/4]
+    # Show image
+    imgcont = np.full((img.shape[0], img.shape[1], 3), 255, dtype=np.uint8)  # create
+    [cv.drawContours(imgcont, [contours[i]], 0, (0,0,0), 5) for i in listindex]
 
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)  # меняем цветовую модель с BGR на HSV
-    thresh = cv.inRange(hsv, hsv_min, hsv_max)  # применяем цветовой фильтр
-    # ищем контуры и складируем их в переменную contours
-    contours, hierarchy = cv.findContours(thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-    # отображаем контуры поверх изображения
-    cv.drawContours(img, contours, -1, (255, 0, 0), 3, cv.LINE_AA, hierarchy, 1)
-    return img
+    return imgcont
 
 
 bot = telebot.TeleBot('911809193:AAFhLS5-OSKrYaX0NcWV5V6N5bX1asfrACc')
